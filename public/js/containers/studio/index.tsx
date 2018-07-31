836003457fdc623c425b04f6cc7dfaeae8addfb9
liufeixiang <liufeixiang@bytedance.com>
2018-07-31 17:16:30 +0800

    update lint-staged
    Change-Id: Ia1080cbf80be0ccd6351a0f01fced6bfe5e96fd3

--- public/js/containers/studio/index.tsx
+++ public/js/containers/studio/index.tsx
@@ -1,12 +1,8 @@
-import React, { Component } from 'react';
-import { Menu, Icon } from 'antd';
+import React, { Component } from "react";
+import { Menu, Icon } from "antd";
 
-interface Props {
-
-}
-interface States {
-
-}
+interface Props {}
+interface States {}
 
 const SubMenu = Menu.SubMenu;
 const Item = Menu.Item;
@@ -15,19 +11,31 @@ export default class Studio extends Component<Props, States> {
   render() {
     return (
       <div className="studio-container">
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={['all']}
-        >
-          <Item key="new_note"><Icon type="plus-circle" />新笔记</Item>
-          <Item key="all_notes"><Icon type="file" />所有笔记</Item>
+        <Menu mode="inline" defaultSelectedKeys={["all"]}>
+          <Item key="new_note">
+            <Icon type="plus-circle" />
+            新笔记
+          </Item>
+          <Item key="all_notes">
+            <Icon type="file" />
+            所有笔记
+          </Item>
           <SubMenu
-            title={<span><Icon type="book" />笔记本</span>}
+            title={
+              <span>
+                <Icon type="book" />
+                笔记本
+              </span>
+            }
             key="notebooks"
           >
-            <Item key="new_notebook"><Icon type="plus-circle-o" />新建笔记本</Item>
+            <Item key="new_notebook">
+              <Icon type="plus-circle-o" />
+              新建笔记本
+            </Item>
           </SubMenu>
         </Menu>
+        <div className="studio-stage">123</div>
       </div>
     );
   }
