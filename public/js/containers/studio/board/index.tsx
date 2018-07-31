00347fe9ead0a68fa942762d381cb9a33232b175
liufeixiang <liufeixiang@bytedance.com>
2018-07-31 18:02:28 +0800

    test
    Change-Id: Ie6fb30d5ca3909331ee398f32f06358e8534133d

---
+++ public/js/containers/studio/board/index.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+interface Props {}
+interface States {}
+
+export default class Board extends Component<Props, States> {
+  render() {
+    return <div className="board-container" />;
+  }
+}
--- public/js/containers/studio/index.tsx
+++ public/js/containers/studio/index.tsx
@@ -11,30 +11,32 @@ export default class Studio extends Component<Props, States> {
   render() {
     return (
       <div className="studio-container">
-        <Menu mode="inline" defaultSelectedKeys={["all"]}>
-          <Item key="new_note">
-            <Icon type="plus-circle" />
-            新笔记
-          </Item>
-          <Item key="all_notes">
-            <Icon type="file" />
-            所有笔记
-          </Item>
-          <SubMenu
-            title={
-              <span>
-                <Icon type="book" />
-                笔记本
-              </span>
-            }
-            key="notebooks"
-          >
-            <Item key="new_notebook">
-              <Icon type="plus-circle-o" />
-              新建笔记本
+        <div className="studio-menu">
+          <Menu mode="inline" defaultSelectedKeys={["all"]}>
+            <Item key="new_note">
+              <Icon type="plus-circle" />
+              新笔记
             </Item>
-          </SubMenu>
-        </Menu>
+            <Item key="all_notes">
+              <Icon type="file" />
+              所有笔记
+            </Item>
+            <SubMenu
+              title={
+                <span>
+                  <Icon type="book" />
+                  笔记本
+                </span>
+              }
+              key="notebooks"
+            >
+              <Item key="new_notebook">
+                <Icon type="plus-circle-o" />
+                新建笔记本
+              </Item>
+            </SubMenu>
+          </Menu>
+        </div>
         <div className="studio-stage">123</div>
       </div>
     );
