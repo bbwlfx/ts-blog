1b0aab06910946db5c95611bce89980558befee5
liufeixiang <liufeixiang@bytedance.com>
2018-08-10 14:57:45 +0800

    add prod webpack.conf
    Change-Id: Iec7e3723198b62e83fb0f8e4520c11d5e6072360

---
+++ public/webpack.prod.config.js
@@ -0,0 +1,18 @@
+const merge = require("webpack-merge");
+const cleanWebpackPlugin = require("clean-webpack-plugin");
+const baseConfig = require("./webpack.client.config");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = merge(baseConfig, {
+  mode: "production",
+  output: {
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "chunk.[name].[chunkhash].js"
+  },
+  plugins: [
+    new cleanWebpackPlugin("dist/client"),
+    new ExtractTextPlugin({
+      filename: "[name].[chunkhash].css",
+      allChunks: true
+    })
+  ]
+});
